f655e910772379af1d84f6e71842cffe88bb6145
Sam Guard <sam.guard@outlook.com>
2024-07-04 20:16:57 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,15 +1,17 @@
-wget https://ipinfo.io/ip -o tmp_ip
+set -e
+wget https://ipinfo.io/ip -O tmp_ip
 if [ $? -ne 0 ]; then
 	echo "Could not get public ip, going to have a bit of a cry..."
 	exit -1
 fi
 
 mv tmp_ip ip
-if [ -z $(git diff) ]; then
+if ! [[ $(git diff ./ip) ]]; then
 	echo "Ip is the same, no update needed"
 	exit 0
 fi
 
+echo "Updating ip to $(cat ip)"
 git add ip
 git commit -m "updated ip"
 # git push origin
